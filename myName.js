4eaed97cd7ed94452ea5a7669f50392d0c7c3c0e
Ibrahimsaidou23 <124426759+Ibrahimsaidou23@users.noreply.github.com>
2023-02-16 13:04:06 +0100

    Add files via upload

---
+++ myName.js
@@ -0,0 +1,3 @@
+let myName="Daouda";
+let myStr="my name is "+  myName  +  " and I am well!";
+console.log(myStr);
---
+++ nameLength.js
@@ -0,0 +1,4 @@
+let nameLength = 0 ;
+const lastname = "Hadiza";
+nameLength = lastname.length ;
+console.log(nameLength);
